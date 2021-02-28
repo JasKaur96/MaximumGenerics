2f315d1ecf7167129b483bbbce7b5b0de4c13e5b
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-28 23:36:09 +0530

    Refactered: Print Max Element

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -1,38 +1,11 @@
 package com.testmaximum;
+package com.testmaximum;
 
-<<<<<<< HEAD
-import java.util.Arrays;
-
-public class TestMaximum{
-    public static Integer[] a= {1,4,5};
-    //Finds max Integer and returns its position.
-    public static int maxInteger()
-    {
-        int max = a[0];
-        for(int i : a) {
-            if(i > max)
-                max = i;
-        }
-        int index = (Arrays.asList(a).indexOf(max)) + 1;
-        System.out.println("\nMaximum value in array is : "+max + " Position : "+index);
-        return index;
-    }
-
-    // prints the array.
-    public static void printIntArray()
-    {
-        for(int i : a)
-           System.out.println(i+ " ");
-    }
-
-    public static void main(String[] args) {
-        printIntArray();
-        maxInteger();
-=======
 public class TestMaximum<T extends Comparable<T>>{
 
     T[] elements;
-
+    
+    //Constructor To store multiple no. of elements.
     public TestMaximum(T ...elements) {
         this.elements = elements;
     }
@@ -50,6 +23,5 @@ public class TestMaximum<T extends Comparable<T>>{
 
     public T maxElement(){
         return TestMaximum.maxElement(elements);
->>>>>>> UC5_PrintMax
     }
 }
