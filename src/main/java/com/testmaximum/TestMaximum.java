82f89d5950344d30a03476ae64429d18a99eae8f
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-28 23:20:30 +0530

    Refactered: Find max element using comparable.

--- src/main/java/com/testmaximum/TestMaximum.java
+++ src/main/java/com/testmaximum/TestMaximum.java
@@ -1,77 +1,26 @@
 package com.testmaximum;
 
-import java.util.Arrays;
+public class TestMaximum<T extends Comparable<T>>{
 
-public class TestMaximum{
-    public static Integer[] a = {1,4,5};
-    public static Float[] b = {1.5f, 8.4f, 2.5f};
-    public static String[] c = {"Sara","Dinithi","aman"};
+    T[] elements;
+
+    public TestMaximum(T ...elements) {
+        this.elements = elements;
+    }
 
     //Finds max Integer and returns its position.
-    public static int maxInteger()
-    {
-        int max = a[0];
-        for(int i : a) {
-            if(i > max)
-                max = i;
+    public static <T extends Comparable<T>> T maxElement(T[] element){
+        T max = element[0];
+        for(T i : element) {
+            if(i.compareTo(max) > 0)
+                max=i;
         }
-        int index = (Arrays.asList(a).indexOf(max)) + 1;
-        System.out.println("\nMaximum value in array is : "+max + " Position : "+index);
-        return index;
+        System.out.println("Maximum value in array is : "+max);
+        return max;
     }
 
-    // prints the Integer array.
-    public static void printIntArray()
-    {
-        for(int i : a)
-           System.out.println(i+ " ");
-    }
 
-    public static int maxFloat()
-    {
-        float max = b[0];
-        for(float i : b) {
-            if(i > max)
-                max = i;
-        }
-        int index = (Arrays.asList(b).indexOf(max)) + 1;
-        System.out.println("\nMaximum value in array is : "+max + " Position : "+index);
-        return index;
-    }
-
-    // prints the Float array.
-    public static void printFloatArray()
-    {
-        for(float i : b)
-            System.out.println(i+ " ");
-    }
-
-    public static int maxString()
-    {
-        String max = c[0];
-        int len = c[0].length();
-        for(String i : c) {
-            if(i.compareTo(max) > len)
-                max = i;
-        }
-        int index = (Arrays.asList(c).indexOf(max)) + 1;
-        System.out.println("\nMaximum value in array is : "+max + " Position : "+index);
-        return index;
-    }
-
-    // prints the String array.
-    public static void printStringArray()
-    {
-        for(String i : c)
-            System.out.println(i+ " ");
-    }
-
-    public static void main(String[] args) {
-        printIntArray();
-        maxInteger();
-        printFloatArray();
-        maxFloat();
-        printStringArray();
-        maxString();
+    public T maxElement(){
+        return TestMaximum.maxElement(elements);
     }
 }
--- src/test/java/com/testmaximum/TestMaximumTestCase.java
+++ src/test/java/com/testmaximum/TestMaximumTestCase.java
@@ -4,59 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestMaximumTestCase {
-    TestMaximum maxTest = new TestMaximum();
+
+    public static Integer[] a = {1,4,5,2,9};
+    public static Float[] b = {1.5f, 8.4f, 2.5f,5.5f};
+    public static String[] c = {"Sara","Dinithi","Aman","Aarya"};
 
     @Test
-    public void maxIntElement_atFirstPosition(){
-        int result = maxTest.maxInteger();
-        Assertions.assertEquals(1,result);
+    public void maxElement_Integer(){
+        TestMaximum<Integer> maxTest = new TestMaximum<Integer>(a);
+        Integer result = maxTest.maxElement();
+        Integer expectedResult = 9;
+        Assertions.assertEquals(expectedResult,result);
     }
 
     @Test
-    public void maxIntElement_atSecondPosition(){
-        int result = maxTest.maxInteger();
-        Assertions.assertEquals(2,result);
+    public void maxElement_Float(){
+        TestMaximum<Float> maxTest = new TestMaximum<Float>(b);
+        Float result = maxTest.maxElement();
+        Float expectedResult = 8.4f;
+        Assertions.assertEquals(expectedResult,result);
     }
 
     @Test
-    public void maxIntElement_atThirdPosition(){
-        int result = maxTest.maxInteger();
-        Assertions.assertEquals(3,result);
-    }
-
-    @Test
-    public void maxFloatElement_atFirstPosition(){
-        int result = maxTest.maxFloat();
-        Assertions.assertEquals(1,result);
-    }
-
-    @Test
-    public void maxFloatElement_atSecondPosition(){
-        int result = maxTest.maxFloat();
-        Assertions.assertEquals(2,result);
-    }
-
-    @Test
-    public void maxFloatElement_atThirdPosition(){
-        int result = maxTest.maxFloat();
-        Assertions.assertEquals(3,result);
-    }
-
-    @Test
-    public void maxStringElement_atFirstPosition(){
-        int result = maxTest.maxString();
-        Assertions.assertEquals(1,result);
-    }
-
-    @Test
-    public void maxStringElement_atSecondPosition(){
-        int result = maxTest.maxString();
-        Assertions.assertEquals(2,result);
-    }
-
-    @Test
-    public void maxStringElement_atThirdPosition(){
-        int result = maxTest.maxString();
-        Assertions.assertEquals(3,result);
+    public void maxElement_String(){
+        TestMaximum<String> maxTest = new TestMaximum<String>("Apple","Peach","Banana","Apricot");
+        String result = maxTest.maxElement(c);
+        String expectedResult = "Sara";
+        Assertions.assertEquals(expectedResult,result);
     }
 }
